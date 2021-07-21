92a738f04a8e3dc5e12970d60a2e2f5110becb5a
hiltoncsj <hiltoncsj@gmail.com>
2021-07-20 23:00:27 -0300

    adding node.js project tabquestions

--- guiaperguntas/index.js
+++ guiaperguntas/index.js
@@ -1,13 +1,24 @@
- var express = require("express");
+const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
-app.get("/", (req, res) => { 
-    res.render("index", {
-        lang: "pt-br",
-        nome: "Hilton Coutinho"
-    }); 
+app.get("/", (req,res) => {
+    res.render("index");
 });
 
-app.listen(8080, () => { console.log("App rodando!"); });
+app.get("/perguntar", (req,res) => {
+    res.render("perguntar");
+});
+
+app.post("/salvarpergunta", (req, res) => {
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send("Título: " + titulo + " - Descrição: " + descricao);
+});
+
+app.listen(8080, () => { console.log("App rodando"); });
